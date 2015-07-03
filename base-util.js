3287322a2b9191188a9d84d8105efe1a0a995fca
Kamran Shahzad <m_kamranshahzad@hotmail.com>
2015-07-03 14:51:59 +0500

    URL helper functions

--- base-util.js
+++ base-util.js
@@ -5,7 +5,35 @@
 var BaseUtil = function(){
 
 
+	/*
+		URL manipulates
+	*/
+	function URL(block_id){
+		this.url_params = [];
+	}
+
+	URL.prototype.addParam = function(param_value){
+		this.url_params.push(param_value);
+	}
+
+	URL.prototype.removeParam = function(param_value){
+		var foundIndex = this.url_params.indexOf(param_value); 
+		if(foundIndex != -1 ){
+			this.url_params.splice(foundIndex,1);
+		}
+	}
+
+	URL.prototype.toString = function(delims){
+		var delims = delims || '-';
+		return this.url_params.join(delims);
+	}
+
+
+
+
 	//public functions
-	return {};
+	return {
+		url : URL
+	};
 
 }({});
